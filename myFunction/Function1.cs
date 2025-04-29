e0f303a76b0913da489e776b64d111211423c77c
User <User@DESKTOP-5ND0PU1>
2025-04-29 10:03:03 +0400

    Add project files.

---
+++ myFunction/Function1.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace myFunction
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Function1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions LEVEL!");
+        }
+
+        [Function("Function2")]
+        public IActionResult Run2([HttpTrigger(AuthorizationLevel.Admin, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions! Admin LEVEL");
+        }
+
+        [Function("Function3")]
+        public IActionResult Run3([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!Anonymous LEVEL");
+        }
+    }
+}
